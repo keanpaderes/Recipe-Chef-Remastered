abd83186fa5d8e88df03309abe7d78f26c196a93
keanpaderes <kean.paderes@gmail.com>
2016-03-29 06:51:07 +0800

    Added delete function and removed some modules.

--- home/home.js
+++ home/home.js
@@ -11,11 +11,22 @@ angular.module('myApp.home', ['ngRoute', 'firebase'])
 }])
 
 // Home controller
-.controller('HomeCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+.controller('HomeCtrl', ['$scope', '$firebaseArray', '$firebaseObject', function($scope, $firebaseArray, $firebaseObject) {
+    //References for Firebase db
     var categoryRefs = new Firebase("https://boiling-fire-2809.firebaseio.com/categories");
     var recipeRefs = new Firebase("https://boiling-fire-2809.firebaseio.com/recipes");
+
+    //Initialization of the hide/show html elements
     $scope.callFormPanel = true;
     $scope.formPanel = true;
+
+    //Form checkers
+    $scope.isAddCatError = false;
+    $scope.hasName = false;
+    $scope.hasCat = false;
+    $scope.hasInfo = false;
+
+    //Array of data from db.
     $scope.categories = $firebaseArray(categoryRefs);
     $scope.recipes = $firebaseArray(recipeRefs);
 
@@ -31,14 +42,25 @@ angular.module('myApp.home', ['ngRoute', 'firebase'])
     }
 
     $scope.addCategory = function() {
+      if($scope.newCategory.length == 0){
+        $scope.isAddCatError = true;
+      }else{
         $scope.categories.$add({
           value: $scope.newCategory
         });
         $scope.recipeCategory = "";
         $scope.newCategory = "";
+        $scope.isAddCatError = false;
+      }
     }
 
     $scope.addRecipe = function() {
+      var isFormNotValid = $scope.recipeCategory == null || $scope.recipeInfo == null || $scope.recipeInfo == null;
+      if(isFormNotValid){
+        $scope.hasName = true;
+        $scope.hasCat = true;
+        $scope.hasInfo = true;
+      }else{
         $scope.recipes.$add({
           recipeCategory: $scope.recipeCategory,
           recipeId: $scope.recipes.length,
@@ -50,5 +72,11 @@ angular.module('myApp.home', ['ngRoute', 'firebase'])
         $scope.recipeName = "";
         $scope.formPanel = !$scope.formPanel
         $scope.callFormPanel = !$scope.callFormPanel
+      }
+    }
+
+    $scope.removeRecipe = function(recipe) {
+      $scope.recipes.$remove(recipe);
+      //$scope.categories.$remove(recipe);
     }
 }]);
